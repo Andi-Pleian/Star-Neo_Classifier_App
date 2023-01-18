0c0bac638126d9114146be5edcad0d3a908789a6
Pleian Andrei Mihai <andreimihai.pleian@gmail.com>
2023-01-18 22:18:54 +0200

    Added Backend

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 # IMPORTS
-from flask import Flask
+
+from flask import Flask, redirect, url_for, render_template, request, jsonify
 from joblib import dump, load
 import pandas as pd
 import numpy as np
@@ -11,6 +12,113 @@ from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn import tree
 
+# Load Star Classifier Model
+star_clf = load('Star_Classifier_Model/Star_Classifier.joblib')
+
+# Load Neo Classifier Model
+neo_clf = load('Neo_Classifier/Neo_Classifier.joblib')
+
+# Get data from frontend
+
+
+app = Flask(__name__)
+
+'''
+        id = request.json['id']
+    name = request.json['name']
+    estDiameterMin = request.json['estDiameterMin']
+    estDiameterMax = request.json['estDiameterMax']
+    relativeVelocity = request.json['relativeVelocity']
+    missDistance = request.json['missDistance']
+    orbitingObject = request.json['orbitingObject']
+    sentryObject = request.json['sentryObject']
+    absoluteMagnitude = request.json['absoluteMagnitude']
+'''
+
+@app.route('/neo', methods=["POST"])
+def neo():
+    id = {'id': request.json['id']}
+    name = {'name': request.json['name']}
+    estDiameterMin = {'estDiameterMin': request.json['estDiameterMin']}
+    estDiameterMax = {'estDiameterMax': request.json['estDiameterMax']}
+    relativeVelocity = {'relativeVelocity': request.json['relativeVelocity']}
+    missDistance = {'missDistance': request.json['missDistance']}
+    orbitingObject = {'orbitingObject': request.json['orbitingObject']}
+    sentryObject = {'sentryObject': request.json['sentryObject']}
+    absoluteMagnitude = {'absoluteMagnitude': request.json['absoluteMagnitude']}
+
+    neo_data = [id['id'], name['name'], estDiameterMin['estDiameterMin'], estDiameterMax['estDiameterMax'], relativeVelocity['relativeVelocity'], missDistance['missDistance'],
+            orbitingObject['orbitingObject'], sentryObject['sentryObject'], absoluteMagnitude['absoluteMagnitude']]
+
+    print(neo_data)
+
+    input_df = pd.DataFrame([neo_data], columns=['id', 'name', 'est_diameter_min', 'est_diameter_max',
+                                           'relative_velocity', 'miss_distance', 'orbiting_body', 'sentry_object',
+                                           'absolute_magnitude'])
+
+    input_df = input_df.drop([ "sentry_object", "orbiting_body", "id", "name"], axis=1)
+
+    neo_prediction = neo_clf.predict(input_df)
+
+    return jsonify({'result': str(neo_prediction)})
+
+
+@app.route('/stars', methods=["POST"])
+def star():
+    temperature = {'temperature': request.json['temperature']}
+    relativeLuminosity = {'relativeLuminosity': request.json['relativeLuminosity']}
+    relativeRadius = {'relativeRadius': request.json['relativeRadius']}
+    absoluteMagnitude = {'absoluteMagnitude': request.json['absoluteMagnitude']}
+    color = {'color': request.json['color']}
+    spectralClass = {'spectralClass': request.json['spectralClass']}
+
+    star_data = [temperature['temperature'],
+                 relativeLuminosity['relativeLuminosity'],
+                 relativeRadius['relativeRadius'],
+                 absoluteMagnitude['absoluteMagnitude'],
+                 color['color'],
+                 spectralClass['spectralClass']]
+
+    print(star_data)
+
+    input_df = pd.DataFrame([star_data], columns=['Temperature',
+                                                  'L',
+                                                  'R',
+                                                  'A_M',
+                                                  'Color',
+                                                  'Spectral_Class'])
+
+    input_df = input_df.drop(["Color", "Spectral_Class"], axis=1)
+
+    star_prediction = star_clf.predict(input_df)
+
+    return jsonify({'result': str(star_prediction)})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
 # path to dataset
 data = 'Star_Classifier_Data/Stars.csv'
 
@@ -42,29 +150,10 @@ test_values_y = test_values["Type"]
 
 # !!! TO BE REMOVED UPON IMPLEMENTING OF KEYBOARD INPUT !!!
 
-# Load Star Classifier Model
-clf = load('Star_Classifier_Model/Star_Classifier.joblib')
-
 # Make predictions
 predictions = clf.predict(test_values_X)
 
 # Compute Accuracy Score
 score = accuracy_score(test_values_y, predictions)
 
-
-app = Flask(__name__)
-
-@app.route('/')
-def hello():
-    # return list of predictions
-    return predictions.tolist()
-
-@app.route('/score')
-def model_score():
-    # return accuracy score
-    return str(score)
-
-@app.route('/y')
-def y_values():
-    # return list of y
-    return test_values_y.tolist()
+'''
